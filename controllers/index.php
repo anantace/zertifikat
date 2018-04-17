3beff1b75faa8b7a4853a4c72d814d8d3e39ea7d
asudau <sudau@elan-ev.de>
2018-04-17 14:41:00 +0200

    bessere Admin-Uebersicht mit mehr Aktionen

--- controllers/index.php
+++ controllers/index.php
@@ -36,6 +36,15 @@ class IndexController extends StudipController {
         }
         $this->dozent = $perm->have_studip_perm('tutor', $this->course_id);
                 //ContactForm::findBySQL
+        
+        $seminar = new Seminar($this->course_id);
+        $this->members = $seminar->getMembers($status= 'autor');
+        
+        foreach ($this->members as $member){
+            $this->members[$member['user_id']]['mail_sent'] = ZertifikatSentEntry::findOneBySQL('user_id = :user_id AND course_id = :course_id', array(':course_id' => $this->course_id, ':user_id' => $member['user_id']));
+        }
+        
+        
     }
 
      public function save_action(){
@@ -76,4 +85,109 @@ class IndexController extends StudipController {
 
         return PluginEngine::getURL($this->dispatcher->plugin, $params, join('/', $args));
     }
+    
+     public function sendMail_action($user_id)
+    {
+
+        $seminar_id = Course::findCurrent()->seminar_id;
+        $course = new Seminar($seminar_id);
+        $institute = new Institute($course->getInstitutId());
+        $zertifikatConfigEntry = ZertifikatConfigEntry::findOneBySQL('course_id = ?', array($seminar_id));
+        $contact_mail = $zertifikatConfigEntry->getValue('contact_mail');
+        
+        $filepath = $this->pdf_action($user, $course->name, $institute->name);
+
+        $dateien = array($filepath);
+        
+        $mailtext = '<html>
+          
+
+            <body>
+
+            <h2>Teilnahmezertifikat für ' . $user . ':</h2>
+
+            <p>Im Anhang finden Sie ein Teilnahmezertifikat für den/die Teilnehmer/in einer Onlineschulung</p>
+
+            </body>
+            </html>
+            ';
+
+            $empfaenger = $contact_mail;//$contact_mail; //Mailadresse
+            //$absender   = "asudau@uos.de";
+            $betreff    = "Teilnahmezertifikat für " . $user . " für erfolgreiche Teilnahme an Mitarbeiterschulung";
+            $filename = 'zertifikat_'. $this->clear_string($user) . '.pdf';
+
+            $mail = new StudipMail();
+            $sent =  $mail->addRecipient($empfaenger)
+                //->addRecipient('elmar.ludwig@uos.de', 'Elmar Ludwig', 'Cc')
+                 ->setReplyToEmail('')
+                 ->setSenderEmail('')
+                 ->setSenderName('E-Learning - DSO - Datenschutz')
+                 ->setSubject($betreff)
+                 ->addFileAttachment($filepath, $name = $filename)
+                 ->setBodyHtml($mailtext)
+                 ->setBodyHtml(strip_tags($mailtext))  
+                 ->send();
+ 
+            if ($sent){
+                PageLayout::postMessage(MessageBox::success(sprintf(_('e-Mail gesendet'), $sem_name)));
+                $this->redirect('index');
+            } else {
+                 PageLayout::postMessage(MessageBox::success(sprintf(_('Senden der eMail fehlgeschlagen'), $sem_name)));
+                $this->redirect('index');
+            }
+            
+    }
+    
+    
+    private function pdf_action($user, $seminar, $institute)
+    {
+        global $STUDIP_BASE_PATH, $TMP_PATH;
+        require_once $STUDIP_BASE_PATH.'/vendor/tcpdf/tcpdf.php';
+        require_once $STUDIP_BASE_PATH.'/public/plugins_packages/elan-ev/Zertifikats_Plugin/models/zertifikatpdf.class.php';
+     
+        
+        // create new PDF document
+        $pdf = new zertifikatpdf(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'ISO-8859-1', false);
+        $pdf->SetTopMargin(40);
+        $pdf->SetLeftMargin(20);
+        $pdf->SetRightMargin(20);
+        $pdf->AddPage();
+        $x = 35;
+        $y = 50;
+        $w = 60;
+        $h = 60;
+        
+        
+            //$html = $this->htmlentitiesOutsideHTMLTags($note_content[0], ENT_HTML401);
+            $html = 'Hiermit wird bescheinigt, dass <br><br><br><br><b>Herr/Frau '. $user 
+                    . '</b><br><br><br>am ' . date("d.m.Y",time()) 
+                    . '<br><br><br>an der Mitarbeiterschulung der Fa. ' . $institute
+                    . '</b><br><br><br>zum Thema<br>'
+                    . '<h1 style="text-align:center">' . $seminar . '</h1>'
+                    . '<br><br><br><br><br>erfolgreich teilgenommen hat.'
+                    . '<br><br><br><br><br><br><br><br>Stephan Beume<br>'
+                    . 'Rechtsanwalt<br>'
+                    . 'Fachanwalt für Arbeitsrecht<br>'
+                    . 'Datenschutzbeauftragter (TÜV)<br>';
+            $pdf->writeHTMLCell('0', '0', '30', '80', studip_utf8encode($html), false, 0, false, 0);
+        
+        $fileid = time();   
+        //$pdf->Output('/tmp/zertifikat'. $fileid, 'F');
+        //return '/tmp/zertifikat'. $fileid;
+        $pdf->Output( $TMP_PATH .'/zertifikat' . $fileid, 'F');
+        return $TMP_PATH . '/zertifikat' . $fileid;
+        //exit("delivering pdf file");
+    }
+    
+    
+     function clear_string($str){
+        $search = array("ä", "ö", "ü", "ß", "Ä", "Ö",
+                "Ü", "&", "é", "á", "ó", " ");
+        $replace = array("ae", "oe", "ue", "ss", "Ae", "Oe",
+                 "Ue", "und", "e", "a", "o", "_");
+        $str = str_replace($search, $replace, $str);
+        //$str = strtolower(preg_replace("/[^a-zA-Z0-9]+/", trim($how), $str));
+        return $str;
+}
 }
--- views/index/index.php
+++ views/index/index.php
@@ -5,15 +5,35 @@
 <body>
 <h1>Mail für das Versenden von Zertifikaten</h1>
 <form method="post" action="<?=$controller->url_for('/index/save')?>">
-    <p><label>E-Mail:<br><input value="<?= $mail?>" type="text" name="Mail"></label></p>
-    <p> Sobald ein/e Teilnehmer/in alle Lerninhalte abgeschlossen, hat wird ein Teilnehmezertifikat generiert und an die hier hinterlegte E-Mail-Adresse versendet.</p>
+    <p><label>E-Mail:<br><input style="width:200px" value="<?= $mail?>" type="text" name="Mail"></label><button type="submit" name="submit" > Speichern </button></p>
+    <p> Sobald ein/e Teilnehmer/in alle Lerninhalte abgeschlossen, hat wird ein Teilnahmezertifikat generiert und an die hier hinterlegte E-Mail-Adresse versendet.</p>
 <!--<p><label>Name:<br><input type="text" name="Name"></label></p>
 <p><label>Betreff:<br><input type="text" name="Betreff"></label></p>
 <p><label>Nachricht:<br>
 <textarea name="Nachricht" cols="50" rows="8"></textarea></label></p>
 -->
-<button type="submit" name="submit" > Speichern
 </form>
+
+<table class='default'>
+    <thead>
+		<tr>
+        <th style='width:10%'><span>Name</span></th>
+        <th style='width:10%'><span>Zertifikat generiert und versendet</span></th>
+        <th style='width:10%'>Aktionen</th>
+        <!--<th>Courseware besucht?</th>-->
+    </tr>
+    </thead>
+<?php foreach ($members as $member){ ?>
+    <tr>
+        
+            <td><?= $member['Vorname'] . ' ' . $member['Nachname']?></td>
+            <td><?= $member['mail_sent'] ? 'Ja' : 'Nein' ?></td>
+            <td><?= $member['mail_sent'] ? "<a href='". $this->controller->url_for('index/sendMail/' . $member['user_id']) ."'>Zertifikat erneut versenden</a><br/>" :"" ?>
+                    <a href='<?= URLHelper::getLink("plugins.php/courseware/progress", array('uid' => $member['user_id'])) ?>' >Fortschritt ansehen</a></td>
+        </tr>
+<?php } ?>
+</table>
+
 </body>
 
 
